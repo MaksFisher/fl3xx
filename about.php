42b25c4ec8d3b4a972391835ba22ebe10f799bda
Maks Fisher <adriel.black666@gmail.com>
2020-10-10 14:31:38 +0300

    add complite desktop about page

--- about.php
+++ about.php
@@ -122,6 +122,33 @@
     </div>
 </section>
 
+<section class="good-section call-to-action">
 
-<div id="otstup"></div>
+    <img src="./images/home-intro.jpg" alt="" class="good-bg img-full-fit">
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-9">
+                <div class="good-companie">
+
+                    <h2 class="good-companie__title good-companie__title--start">Interested in our latest chapter? </h2>
+
+                    <p class="good-companie__subtext good-companie__subtext--start">Learn more about FL3XX products</p>
+
+                    <div class="text-center">
+                        <a href="#" class="btn btn-secondary mt-3">
+                            <span class="btn__text">Get a free demo</span>
+                            <div class="btn__icon">
+                                <svg width="15" height="9">
+                                    <use xlink:href="#arrow-icon"></use>
+                                </svg>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
 <?php require_once('views/base/footer.php'); ?>
